744a530563cceec32832ce235b6fcd84d901cf13
net-ka <a.k.fedosova@gmail.com>
2020-01-12 03:15:25 +0200

    init: add basic structure

---
+++ simple-store-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import './store';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      1234567
+    </div>
+  );
+}
+
+export default App;
