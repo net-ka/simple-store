7a5122fbf3cbfa859c1102900e2b7fea66fc6de0
net-ka <a.k.fedosova@gmail.com>
2020-01-20 04:44:34 +0200

    Updates

---
+++ precache-manifest.537e76752dc620bdd9ba101bcadeeb39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4da794bdfc77a01b9d357b57177284",
+    "url": "/simple-store/index.html"
+  },
+  {
+    "revision": "f7fe85d3eb4c9a0ffa55",
+    "url": "/simple-store/static/css/main.f4009c6e.chunk.css"
+  },
+  {
+    "revision": "5404a1bacc3c5cf85553",
+    "url": "/simple-store/static/js/2.6a02f51b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/simple-store/static/js/2.6a02f51b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7fe85d3eb4c9a0ffa55",
+    "url": "/simple-store/static/js/main.d1d4b632.chunk.js"
+  },
+  {
+    "revision": "2ecd4e69f565f12a7a62",
+    "url": "/simple-store/static/js/runtime-main.3f18a541.js"
+  }
+]);
